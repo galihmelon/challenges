630ff4a370acad3dde223ce1187310bdf9737b5b
Dianing Galih Yudono <galih@Galihs-Laptop.local>
2017-03-07 21:40:34 +0000

    Implemented wordvalue-template.py in wordvalue.py

---
+++ 01/wordvalue.py
@@ -0,0 +1,27 @@
+import re
+from data import DICTIONARY, LETTER_SCORES
+
+def load_words():
+    """Load dictionary into a list and return list"""
+    words = []
+    with open(DICTIONARY, 'r') as fin:
+        words = fin.read().splitlines()
+    return words
+
+def calc_word_value(word):
+    """Calculate the value of the word entered into function
+    using imported constant mapping LETTER_SCORES"""
+    return sum(
+        LETTER_SCORES[c] for c in word.upper()
+        if c in LETTER_SCORES
+        )
+
+def max_word_value(words=None):
+    """Calculate the word with the max value, can receive a list
+    of words as arg, if none provided uses default DICTIONARY"""
+    if words is None:
+        words = load_words()
+    return max(words, key=calc_word_value)
+
+if __name__ == "__main__":
+    pass # run unittests to validate
